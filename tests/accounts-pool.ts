6cbb2a6c436cbac03d3b8960d0e60ab12b046d7d
bogdan.poznyak <bogdan.poznyak@whalemap.io>
2022-08-19 18:08:13 +0300

    generate keypairs for tests

---
+++ tests/accounts-pool.ts
@@ -0,0 +1,23 @@
+import { web3 } from "@project-serum/anchor";
+
+function createKeypairs(count: number): web3.Keypair[] {
+    let keypairs = [];
+    for (let i = 0; i < count; i++) {
+        keypairs.push(web3.Keypair.generate());
+    }
+
+    return keypairs;
+}
+
+async function airdropSol(connection: web3.Connection, recepient: web3.PublicKey, solAmount: number) {
+    await connection.requestAirdrop(recepient, solAmount * 1e9);
+}
+
+export async function createAndFundAccounts(connection: web3.Connection, count: number, solAmount: number): Promise<web3.Keypair[]> {
+    let keypairs = createKeypairs(count);
+
+    let promises = keypairs.map(async x => await airdropSol(connection, x.publicKey, solAmount));
+    await Promise.all(promises);
+
+    return keypairs;
+}
