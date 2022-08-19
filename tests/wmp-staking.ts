426dab50f15ef0720aeb72cd4570e7d5610586fd
bogdan.poznyak <bogdan.poznyak@whalemap.io>
2022-08-19 18:08:35 +0300

    test initialize

--- tests/wmp-staking.ts
+++ tests/wmp-staking.ts
@@ -1,15 +1,38 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, web3 } from "@project-serum/anchor";
+import { getInitializeAccounts } from "../app/program/accounts";
 import { WmpStaking } from "../target/types/wmp_staking";
+import { createAndFundAccounts } from "./accounts-pool";
 
 describe("wmp-staking", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.WmpStaking as Program<WmpStaking>;
+  let keypairs: web3.Keypair[] = [];
+  let adminKeyPair: web3.Keypair;
+  let aliceKeyPair: web3.Keypair;
+  let bobKeyPair: web3.Keypair;
+
+  before(async () => {
+    keypairs = await createAndFundAccounts(program.provider.connection, 10, 0.5);
+    adminKeyPair = keypairs[0];
+    aliceKeyPair = keypairs[1];
+    bobKeyPair = keypairs[2];
+
+    console.log("Generated accounts:")
+    keypairs.forEach(x => 
+      console.log(x.publicKey.toBase58()))
+  });
 
   it("initialize works", async () => {
-    const tx = await program.methods.initialize().rpc();
+    let accounts = await getInitializeAccounts(adminKeyPair.publicKey);
+    const tx = await program.methods
+      .initialize()
+      .accounts(accounts)
+      .signers([adminKeyPair])
+      .rpc({skipPreflight: true});
+
     console.log("Your transaction signature", tx);
   });
 
