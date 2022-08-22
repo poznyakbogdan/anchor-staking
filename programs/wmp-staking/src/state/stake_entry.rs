e9f9b89a9b5c2a58fa0b88275f328346e331d7ac
bogdan.poznyak <bogdan.poznyak@whalemap.io>
2022-08-22 14:31:14 +0300

    impl create_stake_entry instruction

---
+++ programs/wmp-staking/src/state/stake_entry.rs
@@ -0,0 +1,13 @@
+use std::mem::size_of;
+
+use anchor_lang::{prelude::*};
+
+pub const STAKE_ENTRY_SIZE: usize = size_of::<StakeEntry>() + 8;
+pub const STAKE_ENTRY_PREFIX: &str = "stake-entry";
+
+#[account]
+pub struct StakeEntry {
+    pub bump: u8,
+    pub pool: Pubkey,
+    pub balance: u64
+}
